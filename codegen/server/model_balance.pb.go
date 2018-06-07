5c4d8a4ca7f7240273bc37c31b5da59fa1e1770b
kaifei@bianjie.ai <kaifei@bianjie.ai>
2018-06-07 17:52:53 +0800

    modify balance response

--- codegen/server/model_balance.pb.go
+++ codegen/server/model_balance.pb.go
@@ -37,7 +37,7 @@ func (m *BalanceRequest) Reset()         { *m = BalanceRequest{} }
 func (m *BalanceRequest) String() string { return proto.CompactTextString(m) }
 func (*BalanceRequest) ProtoMessage()    {}
 func (*BalanceRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_model_balance_ef5f43277c8e9786, []int{0}
+	return fileDescriptor_model_balance_dba89d7c77f60496, []int{0}
 }
 func (m *BalanceRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_BalanceRequest.Unmarshal(m, b)
@@ -64,38 +64,38 @@ func (m *BalanceRequest) GetAddress() string {
 	return ""
 }
 
-type SequenceResponse struct {
+type BalanceResponse struct {
 	Coins                []*Coin  `protobuf:"bytes,1,rep,name=coins" json:"coins,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *SequenceResponse) Reset()         { *m = SequenceResponse{} }
-func (m *SequenceResponse) String() string { return proto.CompactTextString(m) }
-func (*SequenceResponse) ProtoMessage()    {}
-func (*SequenceResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_model_balance_ef5f43277c8e9786, []int{1}
+func (m *BalanceResponse) Reset()         { *m = BalanceResponse{} }
+func (m *BalanceResponse) String() string { return proto.CompactTextString(m) }
+func (*BalanceResponse) ProtoMessage()    {}
+func (*BalanceResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_model_balance_dba89d7c77f60496, []int{1}
 }
-func (m *SequenceResponse) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_SequenceResponse.Unmarshal(m, b)
+func (m *BalanceResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_BalanceResponse.Unmarshal(m, b)
 }
-func (m *SequenceResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_SequenceResponse.Marshal(b, m, deterministic)
+func (m *BalanceResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_BalanceResponse.Marshal(b, m, deterministic)
 }
-func (dst *SequenceResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SequenceResponse.Merge(dst, src)
+func (dst *BalanceResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_BalanceResponse.Merge(dst, src)
 }
-func (m *SequenceResponse) XXX_Size() int {
-	return xxx_messageInfo_SequenceResponse.Size(m)
+func (m *BalanceResponse) XXX_Size() int {
+	return xxx_messageInfo_BalanceResponse.Size(m)
 }
-func (m *SequenceResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_SequenceResponse.DiscardUnknown(m)
+func (m *BalanceResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_BalanceResponse.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_SequenceResponse proto.InternalMessageInfo
+var xxx_messageInfo_BalanceResponse proto.InternalMessageInfo
 
-func (m *SequenceResponse) GetCoins() []*Coin {
+func (m *BalanceResponse) GetCoins() []*Coin {
 	if m != nil {
 		return m.Coins
 	}
@@ -104,21 +104,21 @@ func (m *SequenceResponse) GetCoins() []*Coin {
 
 func init() {
 	proto.RegisterType((*BalanceRequest)(nil), "model.balance.BalanceRequest")
-	proto.RegisterType((*SequenceResponse)(nil), "model.balance.SequenceResponse")
+	proto.RegisterType((*BalanceResponse)(nil), "model.balance.BalanceResponse")
 }
 
-func init() { proto.RegisterFile("model_balance.proto", fileDescriptor_model_balance_ef5f43277c8e9786) }
+func init() { proto.RegisterFile("model_balance.proto", fileDescriptor_model_balance_dba89d7c77f60496) }
 
-var fileDescriptor_model_balance_ef5f43277c8e9786 = []byte{
-	// 153 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_model_balance_dba89d7c77f60496 = []byte{
+	// 150 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xce, 0xcd, 0x4f, 0x49,
 	0xcd, 0x89, 0x4f, 0x4a, 0xcc, 0x49, 0xcc, 0x4b, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
 	0xe2, 0x05, 0x0b, 0xea, 0x41, 0x05, 0xa5, 0x84, 0x20, 0x6a, 0x92, 0xf3, 0x73, 0x73, 0xf3, 0xf3,
 	0x20, 0x4a, 0x94, 0xb4, 0xb8, 0xf8, 0x9c, 0x20, 0xd2, 0x41, 0xa9, 0x85, 0xa5, 0xa9, 0xc5, 0x25,
 	0x42, 0x12, 0x5c, 0xec, 0x89, 0x29, 0x29, 0x45, 0xa9, 0xc5, 0xc5, 0x12, 0x8c, 0x0a, 0x8c, 0x1a,
-	0x9c, 0x41, 0x30, 0xae, 0x92, 0x0d, 0x97, 0x40, 0x30, 0x48, 0x11, 0x58, 0x71, 0x71, 0x41, 0x7e,
-	0x5e, 0x71, 0xaa, 0x90, 0x06, 0x17, 0x6b, 0x72, 0x7e, 0x66, 0x1e, 0x48, 0x2d, 0xb3, 0x06, 0xb7,
-	0x91, 0x90, 0x1e, 0xc4, 0x4a, 0xa8, 0x1d, 0xce, 0xf9, 0x99, 0x79, 0x41, 0x10, 0x05, 0x4e, 0xec,
-	0x51, 0xac, 0x60, 0xb9, 0x00, 0x86, 0x24, 0x36, 0xb0, 0xdd, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff,
-	0xff, 0x99, 0x01, 0xd9, 0x0e, 0xb5, 0x00, 0x00, 0x00,
+	0x9c, 0x41, 0x30, 0xae, 0x92, 0x35, 0x17, 0x3f, 0x5c, 0x6d, 0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa,
+	0x90, 0x06, 0x17, 0x6b, 0x72, 0x7e, 0x66, 0x1e, 0x48, 0x29, 0xb3, 0x06, 0xb7, 0x91, 0x90, 0x1e,
+	0xc4, 0x46, 0xa8, 0x15, 0xce, 0xf9, 0x99, 0x79, 0x41, 0x10, 0x05, 0x4e, 0xec, 0x51, 0xac, 0x60,
+	0xb9, 0x00, 0x86, 0x24, 0x36, 0xb0, 0xd5, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xb8, 0x95,
+	0x3e, 0xcb, 0xb4, 0x00, 0x00, 0x00,
 }
